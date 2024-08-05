79d5ee5ef608e745bfc9aebd24e0eec27c205358
BalanGabriel <balan_gabriel1999@yahoo.com>
2024-08-05 21:59:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send(`

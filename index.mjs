b34c1a34ed20fabbbfbcb6164a5ef293002cb37f
BalanGabriel <balan_gabriel1999@yahoo.com>
2024-08-06 17:50:39 +0300

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 import cors from 'cors';
-import { OpenAI } from 'openai';
+import OpenAI from 'openai';
 
 const app = express();
 const port = process.env.PORT || 3000;
